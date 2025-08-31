9ef47b7dff95303784a6c3e661b3cb8418b6b559
Aryan <prayogoaryan63@example.com>
2025-08-31 10:34:08 +0700

    Chatbot: only render on main site; exclude /admin and /login routes

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,15 +2,19 @@ import '../styles/globals.css';
 import { PerformanceProvider } from '../components/PerformanceContext';
 import { DataProvider } from '../components/DataContext';
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
 
 const Chatbot = dynamic(() => import('../components/Chatbot'), { ssr: false });
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const path = router?.pathname || '';
+  const isAdminArea = /^\/(?:admin|login)(?:$|\/)/i.test(path);
   return (
     <PerformanceProvider>
       <DataProvider>
   <Component {...pageProps} />
-  <Chatbot />
+  {!isAdminArea && <Chatbot />}
       </DataProvider>
     </PerformanceProvider>
   );
